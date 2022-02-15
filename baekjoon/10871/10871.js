5c8da8a20743464b6269b73457e7d883ff484543
wem519 <wem519@gmail.com>
2022-02-16 05:06:28 +0900

    폴더 정리

--- baekjoon/10871/10871.js
+++ baekjoon/10871/10871.js
@@ -6,10 +6,11 @@ function solution(input) {
   let num = input[0].split(" ").map(e => Number(e));
   let arr = input[1].split(" ").map(e => Number(e));
   let small = []
+    
     for ( let i = 0; i <=num[0]; i++) {
-      if( arr[i] < num[1]) {
-        small.push(arr[i]);
-     }
+        if( arr[i] < num[1]) {
+          small.push(arr[i]);
+        }
     }
     return small.join(" ")
 }
--- baekjoon/2753/2753.js
+++ baekjoon/2753/2753.js
@@ -3,12 +3,12 @@ let input = fs.readFileSync('example.txt').toString().split("\n");
 console.log(input)
 
 function solution(input) {
-const year = Number(input[0])
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    return "1";
-  } else {
-    return "0";
-  }
+  const year = Number(input[0])
+    if ((year % 4 === 0 && year % 100 !== 0) || year %  400 === 0) {
+    return "1";   
+    } else {
+      return "0";
+    }
 }
 console.log(solution(input))
 console.log(`${solution(2022)}`)
