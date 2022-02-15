77c8e407f59bd8e3e5ba030150cceb00a82c541f
wem519 <wem519@gmail.com>
2022-02-15 13:57:01 +0900

    10871문제 테스트

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // 백준 10871문제 테스트
 
-var fs = require('fs');
-var input = fs.readFileSync('example.txt').toString().split("\n");
-
-let num = input[0].split(" ").map(e => Number(e));
-let arr = input[1].split(" ").map(e => Number(e));
-let small = []
-for ( let i = 0; i <=num[0]; i++) {
-  if( arr[i] < num[1]) {
-    small.push(arr[i]);
-  }
+  var fs = require('fs');
+  var input = fs.readFileSync('example.txt').toString().split("\n");
+  console.log(input)
+  const solution = (input) =>{
+  
+  let num = input[0].split(" ").map(e => Number(e));
+  let arr = input[1].split(" ").map(e => Number(e));
+  let small = []
+    for ( let i = 0; i <=num[0]; i++) {
+      if( arr[i] < num[1]) {
+        small.push(arr[i]);
+     }
+    }
+    return small.join(" ")
 }
-console.log(small.join(" "))
+console.log(solution(input));
+
+// 10871문제 테스트 케이스
