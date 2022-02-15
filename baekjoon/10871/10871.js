3529c7fc2b67505339616e421ecb2091c0a0bc2b
wem519 <wem519@gmail.com>
2022-02-16 04:58:13 +0900

    백준 10871문제(x보다 작은 수)

---
+++ baekjoon/10871/10871.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var input = fs.readFileSync('example.txt').toString().split("\n");
+
+function solution(input) {
+  
+  let num = input[0].split(" ").map(e => Number(e));
+  let arr = input[1].split(" ").map(e => Number(e));
+  let small = []
+    for ( let i = 0; i <=num[0]; i++) {
+      if( arr[i] < num[1]) {
+        small.push(arr[i]);
+     }
+    }
+    return small.join(" ")
+}
+console.log(`${solution(['5 20', '12 21 24 50 10'])}`);
+console.log(solution(input))
