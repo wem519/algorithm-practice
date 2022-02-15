d94b36b89939d87acad61a5419a5fecf1e6c99b8
wem519 <wem519@gmail.com>
2022-02-16 05:07:22 +0900

    백준 디렉토리 정리

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
   var fs = require('fs');
   var input = fs.readFileSync('example.txt').toString().split("\n");
-  console.log(input)
+  // console.log(input)
   const solution = (input) =>{
   
   let num = input[0].split(" ").map(e => Number(e));
@@ -15,6 +15,10 @@
     }
     return small.join(" ")
 }
-console.log(solution(input));
+console.log(`${solution(['5 20', '12 21 24 50 10'])}`);
+console.log(solution(input))
 
 // 10871문제 테스트 케이스
+// case1
+// 10 5
+// 1 10 4 9 2 3 8 5 7 6
